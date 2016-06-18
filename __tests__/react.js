ce257addaac8169eaa000bead7c31403cd25dff6
Alan <alan.zanatta@outlook.com>
2016-06-18 14:37:44 +0200

    Adds unit tests for map utils as functions

--- __tests__/react.js
+++ __tests__/react.js
@@ -10,98 +10,197 @@ import TestUtils from 'react-addons-test-utils';
 import RequestStateConnecter from '../lib/react';
 
 describe('React request state connecter', () => {
-  let SuccessComponent = React.createClass({render: () => null});
-  let LoadingComponent = React.createClass({render: () => null});
-  let ErrorComponent = React.createClass({render: () => null});
-  let StateContainer = RequestStateConnecter('data.fetch', {
-    mapStateToComponent: {
-      'PENDING': (<LoadingComponent/>),
-      'SUCCESS': (<SuccessComponent/>),
-      'ERROR': (<ErrorComponent/>),
-      'DEFAULT': (<LoadingComponent/>)
-    },
-    mapRequestIDToProps: {
-      'data.fetch': {
-        download: true
+  describe('with mapStateToComponent and mapRequestIDToProps as objects', () => {
+    let SuccessComponent = React.createClass({render: () => null});
+    let LoadingComponent = React.createClass({render: () => null});
+    let ErrorComponent = React.createClass({render: () => null});
+    let StateContainer = RequestStateConnecter('data.fetch', {
+      mapStateToComponent: {
+        'PENDING': (<LoadingComponent/>),
+        'SUCCESS': (<SuccessComponent/>),
+        'ERROR': (<ErrorComponent/>),
+        'DEFAULT': (<LoadingComponent/>)
+      },
+      mapRequestIDToProps: {
+        'data.fetch': {
+          download: true
+        }
       }
-    }
-  }, {});
-  it('should render the spinner', () => {
-    const ConnectedComponent = TestUtils.renderIntoDocument(
-      <StateContainer reducer={{
+    }, {});
+    it('should render the spinner', () => {
+      const ConnectedComponent = TestUtils.renderIntoDocument(
+        <StateContainer reducer={{
           data: {
             fetch: {
               state: 'PENDING'
             }
           }
         }}/>
-    );
-    expect(TestUtils.findRenderedComponentWithType(ConnectedComponent, LoadingComponent)).toBeDefined();
-  });
+      );
+      expect(TestUtils.findRenderedComponentWithType(ConnectedComponent, LoadingComponent)).toBeDefined();
+    });
 
-  it('should render the error', () => {
-    const ConnectedComponent = TestUtils.renderIntoDocument(
-      <StateContainer reducer={{
+    it('should render the error', () => {
+      const ConnectedComponent = TestUtils.renderIntoDocument(
+        <StateContainer reducer={{
           data: {
             fetch: {
               state: 'ERROR'
             }
           }
         }}/>
-    );
-    expect(TestUtils.findRenderedComponentWithType(ConnectedComponent, ErrorComponent)).toBeDefined();
-  });
+      );
+      expect(TestUtils.findRenderedComponentWithType(ConnectedComponent, ErrorComponent)).toBeDefined();
+    });
 
-  it('should render the success', () => {
-    const ConnectedComponent = TestUtils.renderIntoDocument(
-      <StateContainer reducer={{
+    it('should render the success', () => {
+      const ConnectedComponent = TestUtils.renderIntoDocument(
+        <StateContainer reducer={{
           data: {
             fetch: {
               state: 'SUCCESS'
             }
           }
         }}/>
-    );
-    expect(TestUtils.findRenderedComponentWithType(ConnectedComponent, SuccessComponent)).toBeDefined();
-  });
+      );
+      expect(TestUtils.findRenderedComponentWithType(ConnectedComponent, SuccessComponent)).toBeDefined();
+    });
 
-  it('should add props for fetch request', () => {
-    const ConnectedComponent = TestUtils.renderIntoDocument(
-      <StateContainer reducer={{
+    it('should add props for fetch request', () => {
+      const ConnectedComponent = TestUtils.renderIntoDocument(
+        <StateContainer reducer={{
           data: {
             fetch: {
               state: 'PENDING'
             }
           }
         }}/>
-    );
-    expect(TestUtils.findRenderedComponentWithType(ConnectedComponent, LoadingComponent).props.download).toBe(true);
-  });
+      );
+      expect(TestUtils.findRenderedComponentWithType(ConnectedComponent, LoadingComponent).props.download).toBe(true);
+    });
 
-  it('should render default component', () => {
-    const ConnectedComponent = TestUtils.renderIntoDocument(
-      <StateContainer reducer={{
+    it('should render default component', () => {
+      const ConnectedComponent = TestUtils.renderIntoDocument(
+        <StateContainer reducer={{
           data: {
             upload: {
               state: 'PENDING'
             }
           }
         }}/>
-    );
-    expect(TestUtils.findRenderedComponentWithType(ConnectedComponent, LoadingComponent)).toBeDefined();
-  });
+      );
+      expect(TestUtils.findRenderedComponentWithType(ConnectedComponent, LoadingComponent)).toBeDefined();
+    });
 
-  it('should render null', () => {
-    let renderer = TestUtils.createRenderer();
-    renderer.render(
-      <StateContainer reducer={{
+    it('should render null', () => {
+      let renderer = TestUtils.createRenderer();
+      renderer.render(
+        <StateContainer reducer={{
           data: {
             fetch: {
               state: 'UNKNOWN STATE'
             }
           }
         }}/>
-    );
-    expect(renderer.getRenderOutput()).toBe(null);
+      );
+      expect(renderer.getRenderOutput()).toBe(null);
+    });
+  });
+
+  describe('with mapStateToComponents and mapRequestIDToProps as functions', () => {
+    let SuccessComponent = React.createClass({render: () => null});
+    let LoadingComponent = React.createClass({render: () => null});
+    let ErrorComponent = React.createClass({render: () => null});
+    let StateContainer = RequestStateConnecter('data.fetch', {
+      mapStateToComponent: (state) => ({
+        'PENDING': (<LoadingComponent/>),
+        'SUCCESS': (<SuccessComponent/>),
+        'ERROR': (<ErrorComponent/>),
+        'DEFAULT': (<LoadingComponent/>)
+      }[state]),
+      mapRequestIDToProps: (reqID) => ({
+        'data.fetch': {
+          download: true
+        }
+      }[reqID])
+    }, {});
+    it('should render the spinner', () => {
+      const ConnectedComponent = TestUtils.renderIntoDocument(
+        <StateContainer reducer={{
+          data: {
+            fetch: {
+              state: 'PENDING'
+            }
+          }
+        }}/>
+      );
+      expect(TestUtils.findRenderedComponentWithType(ConnectedComponent, LoadingComponent)).toBeDefined();
+    });
+
+    it('should render the error', () => {
+      const ConnectedComponent = TestUtils.renderIntoDocument(
+        <StateContainer reducer={{
+          data: {
+            fetch: {
+              state: 'ERROR'
+            }
+          }
+        }}/>
+      );
+      expect(TestUtils.findRenderedComponentWithType(ConnectedComponent, ErrorComponent)).toBeDefined();
+    });
+
+    it('should render the success', () => {
+      const ConnectedComponent = TestUtils.renderIntoDocument(
+        <StateContainer reducer={{
+          data: {
+            fetch: {
+              state: 'SUCCESS'
+            }
+          }
+        }}/>
+      );
+      expect(TestUtils.findRenderedComponentWithType(ConnectedComponent, SuccessComponent)).toBeDefined();
+    });
+
+    it('should add props for fetch request', () => {
+      const ConnectedComponent = TestUtils.renderIntoDocument(
+        <StateContainer reducer={{
+          data: {
+            fetch: {
+              state: 'PENDING'
+            }
+          }
+        }}/>
+      );
+      expect(TestUtils.findRenderedComponentWithType(ConnectedComponent, LoadingComponent).props.download).toBe(true);
+    });
+
+    it('should render default component', () => {
+      const ConnectedComponent = TestUtils.renderIntoDocument(
+        <StateContainer reducer={{
+          data: {
+            upload: {
+              state: 'PENDING'
+            }
+          }
+        }}/>
+      );
+      expect(TestUtils.findRenderedComponentWithType(ConnectedComponent, LoadingComponent)).toBeDefined();
+    });
+
+    it('should render null', () => {
+      let renderer = TestUtils.createRenderer();
+      renderer.render(
+        <StateContainer reducer={{
+          data: {
+            fetch: {
+              state: 'UNKNOWN STATE'
+            }
+          }
+        }}/>
+      );
+      expect(renderer.getRenderOutput()).toBe(null);
+    });
   });
 });
