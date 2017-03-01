8f55451289b6c903f3937e84f76d036088466558
alanzanattadev <alan.zanatta@outlook.com>
2017-03-01 16:18:59 +0100

    Fixes webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,18 @@ module.exports = {
     }]
   },
   externals: {
-    react: true,
-  },
+      react: {
+          root: 'React',
+          commonjs2: 'react',
+          commonjs: 'react',
+          amd: 'react'
+      },
+      'react-dom': {
+          root: 'ReactDOM',
+          commonjs2: 'react-dom',
+          commonjs: 'react-dom',
+          amd: 'react-dom'
+      }
+  }
   devtool: 'source-map',
 }
