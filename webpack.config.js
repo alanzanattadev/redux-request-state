df266e354aeba1e8e17310a7e987f4a543c55efe
alanzanattadev <alan.zanatta@outlook.com>
2017-07-27 15:54:24 +0200

    Decreases drastically bundle size

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,57 @@
-var path = require('path');
+var path = require("path");
 
 module.exports = {
-  entry: './lib/index.js',
+  entry: "./lib/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    library: 'ReduxRequestState',
-    libraryTarget: 'umd',
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    library: "ReduxRequestState",
+    libraryTarget: "umd",
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: [/node_modules/],
-      use: [{
-        loader: 'babel-loader',
-      }]
-    }]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
+    ],
   },
   externals: {
-      react: {
-          root: 'React',
-          commonjs2: 'react',
-          commonjs: 'react',
-          amd: 'react'
-      },
-      'react-dom': {
-          root: 'ReactDOM',
-          commonjs2: 'react-dom',
-          commonjs: 'react-dom',
-          amd: 'react-dom'
-      }
+    react: {
+      root: "React",
+      commonjs2: "react",
+      commonjs: "react",
+      amd: "react",
+    },
+    "react-dom": {
+      root: "ReactDOM",
+      commonjs2: "react-dom",
+      commonjs: "react-dom",
+      amd: "react-dom",
+    },
+    immutable: {
+      root: "immutable",
+      commonjs2: "immutable",
+      commonjs: "immutable",
+      amd: "immutable",
+    },
+    recompose: {
+      root: "recompose",
+      commonjs2: "recompose",
+      commonjs: "recompose",
+      amd: "recompose",
+    },
+    "prop-types": {
+      root: "prop-types",
+      commonjs2: "prop-types",
+      commonjs: "prop-types",
+      amd: "prop-types",
+    },
   },
-  devtool: 'source-map',
-}
+  devtool: "source-map",
+};
